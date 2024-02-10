08e227e645e0f22881b7caccce63ac461b989100
alex-liping <157304277+alex-liping@users.noreply.github.com>
2024-02-10 10:34:37 +0100

    Počáteční soubory projektu MakeCode

---
+++ main.ts
@@ -0,0 +1,14 @@
+led.enable(false)
+pins.digitalWritePin(DigitalPin.P0, 0)
+pins.digitalWritePin(DigitalPin.P1, 0)
+pins.digitalWritePin(DigitalPin.P2, 0)
+pins.digitalWritePin(DigitalPin.P3, 0)
+pins.digitalWritePin(DigitalPin.P5, 0)
+pins.digitalWritePin(DigitalPin.P6, 0)
+pins.digitalWritePin(DigitalPin.P7, 0)
+pins.digitalWritePin(DigitalPin.P10, 0)
+pins.digitalWritePin(DigitalPin.P14, 0)
+pins.digitalWritePin(DigitalPin.P16, 0)
+basic.forever(function () {
+	
+})
